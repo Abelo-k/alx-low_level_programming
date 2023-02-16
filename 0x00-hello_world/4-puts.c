f1fbab8126ac043831a92a1f7f4edf258fe6ea74
Abel_k <kebedeabel34@gmail.com>
2023-02-16 06:49:15 -0800

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char str = "Programing is like building a multilingual puzzle \n";
+
+puts(str)
+}
