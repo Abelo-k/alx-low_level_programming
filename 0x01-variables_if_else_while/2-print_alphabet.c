0bfd5f1b10fb006c93d5fa8fecf1804fec01ee03
Abel_k <kebedeabel34@gmail.com>
2023-02-17 05:50:10 -0800

    lower_00

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <time.h>
+/**
+ * main - prints lower case letters
+ *
+ * Return: 0 (success)
+ *
+ */
+int main(void)
+{
+	char a = 'G';
+
+	putchar(tolower(a));
+
+	return (0);
+}
