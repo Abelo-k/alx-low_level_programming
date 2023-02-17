ae32f8ed1bc955b25ad0d52151b54f2c9123e9f1
Abel_k <kebedeabel34@gmail.com>
2023-02-17 10:14:07 -0800

    numberz_00

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - print (0-9) by putchar
+ *
+ * Return: 0(success)
+ *
+ */
+int main(void)
+{
+	int a = 0;
+
+	for (a = 0; a < 9; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
