b10729c9b75d267b52c61b5c93c9e930a497e384
Abel_k <kebedeabel34@gmail.com>
2023-02-17 10:02:39 -0800

    numbers_00

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints (0-9)
+ *
+ * Return: 0(success)
+ *
+ */
+int main(void)
+{
+	int a = 0;
+
+	for (a = 0; a < 10; a++)
+	{
+		printf("%d", a)
+	}
+	printf("\n");
+	return (0);
+}
