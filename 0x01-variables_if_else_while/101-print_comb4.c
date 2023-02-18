1f11dcc545e458bf1e64165676f6c6302923f647
Abel_k <kebedeabel34@gmail.com>
2023-02-17 16:08:26 -0800

    comb4_00

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints combination of three digits without repitition
+ *
+ * Return: 0(success)
+ *
+ */
+int main(void)
+{
+	int a = 0;
+	int b = 0;
+	int c = 0;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = a + 1; b < 10; b++)
+		{
+			for (c = b + 1; c < 10; c++)
+			{
+				putchar(a + '0');
+				putchar(b + '0');
+				putchar(c + '0');
+				if (a == 9 && b == 8 && c == 7)
+					continue;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
