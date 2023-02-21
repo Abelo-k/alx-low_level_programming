4a8669997e60b70f5e454997a186498225d8074f
Abel_k <kebedeabel34@gmail.com>
2023-02-21 15:04:28 -0800

    check_00

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * print_sign - check a number if it is positive or negative or zero
+ * @n: the number that is going to be checked
+ *
+ * Return: 1(positive) or 0(zero) or -1(megative)
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		_putchar(',');
+		_putchar(' ');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		_putchar(',');
+		_putchar(' ');
+		return (-1);
+	}
+	else
+	{
+		_putchar(0 + '0');
+		_putchar(',');
+		_putchar(' ');
+		return (0);
+	}
+}
