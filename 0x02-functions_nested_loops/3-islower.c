d92e6c7d57fedc1876d687cc95fffe8b8bdea2e8
Abel_k <kebedeabel34@gmail.com>
2023-02-21 13:55:09 -0800

    is_lower_00

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * _islower - checks if a letter is lower
+ * @c : the number to be checked
+ *
+ * Return: 1(success)
+ */
+int _islower(int c)
+{
+	if (c == tolower('c'))
+		return (1);
+	else
+		return (0);
+}
