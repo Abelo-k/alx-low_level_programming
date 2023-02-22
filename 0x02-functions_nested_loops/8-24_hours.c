08bac632c7db426bf7c480041289d755c43337be
Abel_k <kebedeabel34@gmail.com>
2023-02-21 16:47:46 -0800

    min_01

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,17 +8,16 @@ void jack_bauer(void)
 	int a;
 	int b;
 
-	for (a = 0; a < 10; a++)
+	for (a = 0; a < 24; a++)
 	{
-		for (b = 0; b < 10; b++)
+		for (b = 0; b < 60; b++)
 		{
-			_putchar((a / 10) + '0'));
-			_putchar((a % 10)  + '0'));
+			_putchar((a / 10) + '0');
+			_putchar((a % 10)  + '0');
 			_putchar(':');
-			if (a == 23 && b == 59)
-				continue;
 			_putchar((b / 10) + '0');
 			_putchar((b % 10) + '0');
+			_putchar('\n');
 		}
 	}
 }
