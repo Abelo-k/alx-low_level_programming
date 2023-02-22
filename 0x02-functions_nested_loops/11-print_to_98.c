8f9ebb9bfe25c6ec63d90b1bed5834c9c6c2a5aa
Abel_k <kebedeabel34@gmail.com>
2023-02-21 18:32:47 -0800

    98_onee

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,8 +13,9 @@ void print_to_98(int n)
 	{
 		for (i = n; i <= 98; i++)
 		{
+			if( i < 98)
 			printf("%d, ", i);
-			if (i == 98)
+			else (i == 98)
 				printf("%d\n", i);
 		}
 	}
@@ -22,8 +23,9 @@ void print_to_98(int n)
 	{
 		for (j = n; j >= 98; j--)
 		{
+			if ( j > 98 )
 			printf("%d, ", j);
-			if (j == 98)
+			else (j == 98)
 				printf("%d\n", j);
 		}
 	}
