33e8446b4111654a3586dcdea652f0e4b845a8c4
Abel_k <kebedeabel34@gmail.com>
2023-02-21 17:43:45 -0800

    times table2

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,22 +14,23 @@ void times_table(void)
 		for (b = 0; b < 10; b++)
 		{
 			c = a * b;
-			if (c > 9)
-			{
-				_putchar((c / 10) + '0');
-				_putchar((c % 10) + '0');
-				if (b == 9)
-					continue;
-				_putchar(',');
-				_putchar(' ');
-			}
-			else
+			if (b == 0)
 			{
 				_putchar(c + '0');
-				if (b == 9)
-					continue;
+			}
+			if (c < 10 && b != 0)
+			{
 				_putchar(',');
 				_putchar(' ');
+				_putchar(' ');
+				_putchar(c + '0');
+			}
+			else if (c >= 10 )
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((c / 10) + '0');
+				_putchar((c % 10) + '0');
 			}
 		}
 		_putchar('\n');
