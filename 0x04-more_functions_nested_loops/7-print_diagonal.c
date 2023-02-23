fe2ca22c5c66b482f7c5be669e4bd59841878523
Abel_k <kebedeabel34@gmail.com>
2023-02-23 10:40:57 -0800

    diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_line - prints n times of _
+ * @n: the amount to print _
+ */
+void print_line(int n)
+{
+	int a;
+	int b;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (a = 1; a <= n ; a++)
+		{
+			for (b = 1; b <= n; b++)
+			{
+				if (b == a)
+					_putchar('\\');
+				else if (b < a)
+					_putchiar('');
+		}
+		_putchar('\n');
+	}
+}
+
