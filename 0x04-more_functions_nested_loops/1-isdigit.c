1677cc2156e1461e89bdb4e0f6a78b0df12496f9
Abel_k <kebedeabel34@gmail.com>
2023-02-23 06:30:58 -0800

    isdigit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -8,7 +8,17 @@
 int _isdigit(int c)
 {
 	if (c >= 0 || c <= 9)
+	{
+		_putchar(c);
+		_putchar(':');
+		_putchar(' ');
 		return (1);
+	}
 	else
+	{
+		_putchar(c);
+		_putchar(':');
+		_putchar(' ');
 		return (0);
+	}
 }
