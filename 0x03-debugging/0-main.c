886c6bb0b148021e672b54710bf96f69a1c262e9
Abel_k <kebedeabel34@gmail.com>
2023-02-23 17:58:36 -0800

    main-0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,19 +1,14 @@
-
-#include "main.h"
-
-
 /**
- * main - tests function
- *
- * Return: 0 success;
- */
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+
 int main(void)
 {
 	int i;
 
 	i = 0;
-	positive_negative(i);
+	positive_or_negative(i);
 
 	return (0);
 }
-
