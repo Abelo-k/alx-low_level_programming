af3a78cf5267408eca852a381fa166c64fde561b
Abel Kebede <abelkebu68@gmail.com>
2023-02-28 00:01:14 +0300

    array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_array - prints elements of array
+ * @n: sizeof array
+ * @a: pointer
+ *
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	int b = n - 1;
+	int num;
+
+	for(num = 0; num < n ; num++)
+	{
+		printf("%d", a[num]);
+		if (num == b)
+			continue;
+		printf(", ");
+	}
+	printf('\n');
+}
