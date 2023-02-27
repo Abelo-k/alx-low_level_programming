672b161c861179100e3bbde61da841d94c7311b0
Abel_k <kebedeabel34@gmail.com>
2023-02-27 14:49:06 -0800

    pass

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - generates valid password
+ *
+ * Return: 0 sucess
+ */
+int main (void)
+{
+	int key;
+	int i, j;
+
+	srand(time(NULL));
+
+	for (i=0; i<=8; i++)
+	{
+		key[i] = rand() % 10;
+	}
+	for (j = 0; j < 8; j++)
+	{
+		printf("%d ", key[j]);
+	}
+	return (0);
+}
