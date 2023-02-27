82fb6cc75e0b837731d053d1716c07ec841c7e42
Abel Kebede <abelkebu68@gmail.com>
2023-02-27 22:33:27 +0300

    jump one

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * puts2 - prints chachters jumping another
+ * @str: string
+ *
+ * Return: void
+ */
+void puts(char *str)
+{
+	char *ch = str;
+	int count = 0;
+	int num;
+
+	while (*ch != '\0')
+	{
+		count++;
+		ch++;
+	}
+	for (num = 0; num < count; num++)
+	{
+		if (num % 2 == 0)
+			_putchar(str[num]);
+	}
+	_putchar('\n');
+}
