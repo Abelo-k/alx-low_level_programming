ea6c13eac33696678bb1d73da64caf9a520bf29b
Abel Kebede <abelkebu68@gmail.com>
2023-02-27 22:02:27 +0300

    rev str

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * rev_string - reverse a string
+ * @s: string
+ *
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int tmp = s[0];
+	int count = 0;
+	int index;
+
+	while (s[count] != '\0')
+	{
+		count++;
+	}
+	for (index = 0; index < count; index--)
+	{
+		count--;
+		tmp = s[index];
+		s[index] = s[count];
+		s[count] = tmp;
+	}
+}
