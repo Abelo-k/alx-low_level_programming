5dc5349b07d80cded62a1302762537c28f8fd754
Abel Kebede <abelkebu68@gmail.com>
2023-02-27 20:49:44 +0300

    count

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strlen - prints length of a string
+ * @s: string
+ *
+ * Return: count
+ */
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while(*s != '\0')
+	{
+		count++;
+		s++;
+	}
+
+	return counnt;
+}
