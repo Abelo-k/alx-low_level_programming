2eda293bc2b9809c950118db3862c107c0c7eb8d
Abel Kebede <abelkebu68@gmail.com>
2023-02-27 23:11:27 +0300

    second half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -16,10 +16,10 @@ void puts_half(char *str)
 		count++;
 		ch++;
 	}
-	if(count % 2 != 0)
+	if (count % 2 != 0)
 		count = count - 1;
 
-	for (num = 0; num < count; num++)
+	for (num = 0; num <= count; num++)
 	{
 		if (num >= count / 2)
 			_putchar(str[num]);
