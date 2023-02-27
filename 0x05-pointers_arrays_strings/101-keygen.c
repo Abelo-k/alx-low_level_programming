5f5dd6d06dd55847ff961ee36840f4cecace2831
Abel_k <kebedeabel34@gmail.com>
2023-02-27 14:58:15 -0800

    pass

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -6,20 +6,26 @@
  *
  * Return: 0 sucess
  */
-int main (void)
+int main(void)
 {
-	int key[8];
-	int i, j;
+	int pass[100];
+	int i, sum, n;
+
+	sum = 0;
 
 	srand(time(NULL));
-
-	for (i=0; i<=8; i++)
+	for (i = 0; i < 100; i++)
 	{
-		key[i] = rand() % 10;
-	}
-	for (j = 0; j < 8; j++)
-	{
-		printf("%d ", key[j]);
+		pass[i] = rand() % 78;
+		sum += (pass[i] + '0');
+		putchar(pass[i] + '0');
+		if ((2772 - sum) - '0' < 78)
+		{
+			n = 2772 - sum - '0';
+			sum += n;
+			putchar(n + '0');
+			break;
+		}
 	}
 	return (0);
 }
