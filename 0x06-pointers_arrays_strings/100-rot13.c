11e88c8e694f068c246181d66401b0639775c656
Abel_k <kebedeabel34@gmail.com>
2023-03-02 12:44:41 -0800

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * rot13 - encoding rot13
+ * @ptr: pointer
+ *
+ * Return: @ptr
+ */
+char *rot13(char *ptr)
+{
+	int i, j;
+
+	char str[] = "ABXDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot13[] = "NOPQRSTUVWXYZABCEDFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; ptr[i] != '\0'; i++)
+	{
+		for (j = 0; rot13[i] != '\0'; j++)
+		{
+			if (ptr[i] == str[j])
+			{
+				ptr[i] == rot13[j];
+				break;
+			}
+		}
+	}
+	return (ptr);
+}
