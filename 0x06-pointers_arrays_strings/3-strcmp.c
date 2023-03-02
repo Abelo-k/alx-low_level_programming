22779b2780f4dd12c03a9c3b8dc94e93e90f6ca5
Abel_k <kebedeabel34@gmail.com>
2023-03-02 08:00:30 -0800

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,11 +8,12 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int a, i;
+	int i;
 
 	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 	{
-		a = (s1 - s2);
+		if(s1[i] != s2[i])
+			return (s1 - s2);
 	}
-	return (a);
+	return (0);
 }
