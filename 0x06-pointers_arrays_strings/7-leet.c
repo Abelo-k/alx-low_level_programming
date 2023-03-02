6e9e8c01ac7dd20bbc105a8ac804b4379e2e44fa
Abel_k <kebedeabel34@gmail.com>
2023-03-02 12:25:50 -0800

    encode

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * leet - encodes string to 1337
+ * @ptr: char pointer
+ *
+ * Return: @ptr
+ */
+char *leet(char *ptr)
+{
+	int i, j;
+
+	char str = "aAeEoOtTlL";
+	char ch = "4433007711";
+
+	for (i = 0; ptr[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (ptr[i] == str[j])
+			{
+				ptr[i] = ch[j];
+			}
+		}
+	}
+	return (ptr);
+}
