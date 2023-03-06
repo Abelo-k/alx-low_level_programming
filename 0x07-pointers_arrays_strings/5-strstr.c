1e0e7020b2ffefb32e366a84ef166371d754fdb2
Abel_k <kebedeabel34@gmail.com>
2023-03-06 15:53:40 -0800

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strstr - locates a string;
+ * @haystack: string 
+ * @needle: string to be located
+ *
+ * Return: @haystack or null
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+
+	while (*haystack)
+	{
+		for (i = 0; haystack[i] != '\0'; i++)
+		{
+			if (haystack == needle[i])
+				return (haystack);
+		}
+		haystack++;
+	}
+	return ('\0');
+}
