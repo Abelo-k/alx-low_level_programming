7668049e0ddc46ff793aaf588e87440ed2a4909f
Abel_k <kebedeabel34@gmail.com>
2023-03-06 16:46:21 -0800

    set string

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * set_string - set the value of a pointer to char
+ * @s: point to apointer
+ * @to: points to a char
+ *
+ * Return: void
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
