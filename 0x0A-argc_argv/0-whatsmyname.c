554f23d482ab8199e236b52500d4707289515842
Abel_k <kebedeabel34@gmail.com>
2023-03-10 07:42:01 -0800

    first commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints name of program
+ * @argc: size of argument
+ * @argv: array of argument
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
