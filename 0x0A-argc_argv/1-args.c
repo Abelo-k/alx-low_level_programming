c142299ba6b688c88e1c29acd428659a4b1e09ce
Abel_k <kebedeabel34@gmail.com>
2023-03-10 08:08:18 -0800

    count

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - counts number of arguments
+ * @argc: size of argument
+ * @argv: array of arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i = 0, count;
+
+	while (i < argc)
+		count++;
+	printf("%d\n", count);
+	return (0);
+}
