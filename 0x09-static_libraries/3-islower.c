61ce3aba62e42a40b1be72ebb021d49444fe11ef
Abel_k <kebedeabel34@gmail.com>
2023-03-10 17:52:22 -0800

    lib

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * _islower - checks if a letter is lower
+ * @c : the number to be checked
+ *
+ * Return: 0(success)
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
